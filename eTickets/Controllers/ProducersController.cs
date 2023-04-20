00f8bc3f1f809e89b9aa603c619b4b952170303d
ben <benoit.saby@hotmail.fr>
2023-04-20 08:50:55 +0200

    Add producer and cinemas controllers

---
+++ eTickets/Controllers/ProducersController.cs
@@ -0,0 +1,19 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class ProducersController : Controller
+    {
+        private readonly AppDbContext _context;
+        public ProducersController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProductors = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
